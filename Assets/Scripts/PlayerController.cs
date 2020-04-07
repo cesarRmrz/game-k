13b195db42a6a2409eb9d28d97141a449bdf107b
cesarRmrz <cesar960106@gmail.com>
2020-04-07 17:57:14 -0500

    Prueba movimiento

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    private Rigidbody2D rb;
+    private Vector2 moveVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            //Metodo para touch
+            // Get movement of the finger since last frame
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+
+            // Move object across XY plane
+            transform.Translate(-touchDeltaPosition.x * speed,
+                        -touchDeltaPosition.y * speed, 0);
+        }
+        else
+        {
+            //Si detecta entrada de teclado usa este metodo
+            Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            moveVelocity = moveInput.normalized * speed;
+            rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+        }
+
+    }
+
+    
+}
